55c720a26e9018ce9f758cd955df85c9e5f70513
Damar Anshary <damar.galihanshary7@gmail.com>
2023-11-30 10:49:28 +0700

    tidying layouts component

--- gen-23.09-react-basic/src/layouts/Layout/index.tsx
+++ gen-23.09-react-basic/src/layouts/Layout/index.tsx
@@ -6,9 +6,7 @@ const Layout = ({children} : LayoutProps) => {
   return (
     <>
       <Navbar />
-      <main className="min-h-screen flex flex-col lg:container gap-y-4 md:px-5 m-5 md:mx-auto md:mt-8">
-        {children}
-      </main>
+      {children}
       <Footer />
     </>
   );
--- gen-23.09-react-basic/src/pages/Home/index.tsx
+++ gen-23.09-react-basic/src/pages/Home/index.tsx
@@ -13,14 +13,17 @@ const Home = () => {
   };
 
   return (
-    <>
+    <main className="min-h-screen flex flex-col xl:container gap-y-4 md:px-5 m-5 md:mx-auto md:mt-8">
       <img
         src="/assets/banner-1.png"
         alt="banner-1"
         className="hidden md:block w-full rounded-lg mb-5"
       />
       {categories.map((category) => (
-        <section key={category} className="flex flex-col gap-y-4 mb-6 pt-4 border border-transparent border-t-gray-200">
+        <section
+          key={category}
+          className="flex flex-col gap-y-4 mb-6 pt-4 border border-transparent border-t-gray-200"
+        >
           <h2 className="text-2xl font-semibold">{category}</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
             {productsData.map((product) => (
@@ -33,7 +36,7 @@ const Home = () => {
           </div>
         </section>
       ))}
-    </>
+    </main>
   );
 };
 
