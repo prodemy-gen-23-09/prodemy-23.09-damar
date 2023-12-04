f3604fc676a3feb766f1d2150a6da99bf2828146
Damar Anshary <damar.galihanshary7@gmail.com>
2023-12-04 07:02:42 +0700

    add login state

--- gen-23.09-react-state/src/layouts/Navbar/index.tsx
+++ gen-23.09-react-state/src/layouts/Navbar/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { IconButton } from "../../components/Button";
 import { IconContext } from "react-icons";
 import {
@@ -10,6 +11,12 @@ import {
 } from "react-icons/ai";
 
 const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <header className="flex flex-row sticky top-0 left-0 z-10 w-full bg-white justify-between md:justify-start items-center border border-x-transparent border-t-transparent py-4 md:py-6 px-3 md:px-4 lg:px-10 xl:px-12 gap-x-5 md:gap-x-8">
       <IconContext.Provider value={{ size: "100%" }}>
@@ -30,32 +37,49 @@ const Navbar = () => {
         placeholder="Cari di tokokami"
       />
       <IconContext.Provider value={{ size: "100%" }}>
-        <nav className="flex flex-row items-center gap-x-5 md:gap-x-8">
-          <IconButton
-            icon={<AiOutlineHeart />}
-            title="wishlist"
-            className="hidden md:block md:h-10"
-          />
-          <IconButton
-            icon={<AiOutlineSearch />}
-            title="search"
-            className="h-8 md:hidden"
-          />
-          <IconButton
-            icon={<AiOutlineShoppingCart />}
-            title="cart"
-            className="h-8 md:h-10"
-          />
-          <IconButton
-            icon={<AiOutlineUser />}
-            title="profile"
-            className="hidden md:block md:h-10"
-          />
-          <IconButton
-            icon={<AiOutlineMenu />}
-            title="menu"
-            className="h-8 md:hidden"
-          />
+        <nav className="flex flex-row items-center gap-x-5">
+          {isLoggedIn ? (
+            <>
+              <IconButton
+                icon={<AiOutlineHeart />}
+                title="wishlist"
+                className="hidden md:block md:h-10"
+              />
+              <IconButton
+                icon={<AiOutlineSearch />}
+                title="search"
+                className="h-8 md:hidden"
+              />
+              <IconButton
+                icon={<AiOutlineShoppingCart />}
+                title="cart"
+                className="h-8 md:h-10"
+              />
+              <IconButton
+                icon={<AiOutlineUser />}
+                title="profile"
+                className="hidden md:block md:h-10"
+              />
+              <IconButton
+                icon={<AiOutlineMenu />}
+                title="menu"
+                className="h-8 md:hidden"
+              />
+            </>
+          ) : (
+            <>
+              {" "}
+              <button
+                className="self-center md:hover:cursor-pointer outline outline-1 outline-green-500 rounded-lg px-6 py-1 h-fit font-bold text-green-500 hover:bg-green-500 hover:text-white"
+                onClick={handleLogin}
+              >
+                Masuk
+              </button>
+              <button className="self-center md:hover:cursor-pointer hover:outline hover:outline-1 hover:outline-green-500 rounded-lg px-6 py-1 h-fit font-bold hover:bg-white hover:text-green-500 bg-green-500 text-white">
+                Daftar
+              </button>
+            </>
+          )}
         </nav>
       </IconContext.Provider>
     </header>
