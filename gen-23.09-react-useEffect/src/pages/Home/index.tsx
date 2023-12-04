6a3ebc03dfb1afd40243a71eeb2d628a624ed8dd
Damar Anshary <damar.galihanshary7@gmail.com>
2023-12-04 20:26:18 +0700

    add sorting effect

--- gen-23.09-react-useEffect/src/pages/Home/index.tsx
+++ gen-23.09-react-useEffect/src/pages/Home/index.tsx
@@ -1,38 +1,73 @@
 import Banner from "../../components/Banner";
 import { ProductCard } from "../../components/Product";
-import { productsData } from "../../data/data";
+import { productsData as productList } from "../../data/data";
+import { ChangeEvent, useEffect, useState } from "react";
 
 const Home = () => {
-  const categories = [
-    "Waktu Indonesia Belanja",
-    "Produk Terbaru",
-    "Rekomendasi untukmu",
-  ];
+  const [productsSortedBy, setProductSortedBy] = useState("Terbaru");
+  const [productsData, setProductsData] = useState(productList);
 
-  const handleOnClick = (productId: number) => {
-    console.log(productId);
+  useEffect(() => {
+    if (productsSortedBy === "Harga tertinggi") {
+      handleSortByHighestPrice();
+    } else if (productsSortedBy === "Harga terendah") {
+      handleSortByLowestPrice();
+    } else if (productsSortedBy === "Terbaru") {
+      handleSortByNewest();
+    }
+  }, [productsSortedBy, productList]);
+
+  const handleSortByHighestPrice = async () => {
+    setProductsData([...productList].sort((a, b) => b.price - a.price));
+  };
+
+  const handleSortByLowestPrice = async () => {
+    setProductsData([...productList].sort((a, b) => a.price - b.price));
+  };
+
+  const handleSortByNewest = async () => {
+    setProductsData(
+      [...productList].sort(
+        (a, b) => b.createdAt.getTime() - a.createdAt.getTime(),
+      ),
+    );
+  };
+
+  const handleOnChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setProductSortedBy(e.target.value);
   };
 
   return (
-    <main className="min-h-screen flex flex-col xl:container gap-y-4 md:px-5 m-5 md:mx-auto md:mt-8">
+    <main className="m-5 flex min-h-screen flex-col gap-y-4 xl:container md:mx-auto md:mt-8 md:px-5">
       <Banner imageUrl="/assets/banner-1.png" />
-      {categories.map((category) => (
-        <section
-          key={category}
-          className="flex flex-col gap-y-4 mb-6 pt-4 border border-transparent border-t-gray-200"
-        >
-          <h2 className="text-2xl font-semibold">{category}</h2>
-          <div className="h-fit grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
-            {productsData.map((product) => (
-              <ProductCard
-                key={product.id}
-                product={product}
-                onClick={handleOnClick}
-              />
-            ))}
-          </div>
-        </section>
-      ))}
+      <section className="mb-6 flex flex-col gap-y-4 border border-transparent border-t-gray-200 pt-4">
+        <div className="flex flex-row items-center justify-between">
+          <h2 className="text-2xl font-semibold">Waktu Indonesia Belanja</h2>
+          <form className="flex flex-row items-center gap-x-2 text-sm">
+            <label htmlFor={`urutkan`}>Urutkan : </label>
+            <select
+              className="rounded-lg border-r-8 border-transparent px-3 py-2 ring-1 ring-gray-200"
+              name={`urutkan`}
+              onChange={(e) => handleOnChange(e)}
+            >
+              <option className="m-1" value="Terbaru">
+                Terbaru
+              </option>
+              <option value="Harga tertinggi">Harga tertinggi</option>
+              <option value="Harga terendah">Harga terendah</option>
+            </select>
+          </form>
+        </div>
+
+        <div className="grid h-fit grid-cols-2 gap-5 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+          {productsData.map((product) => (
+            <ProductCard
+              key={product.id}
+              product={product}
+            />
+          ))}
+        </div>
+      </section>
     </main>
   );
 };
