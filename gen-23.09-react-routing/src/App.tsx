d92ccb011282f8036b5664385597001fd87f24f3
Damar Anshary <damar.galihanshary7@gmail.com>
2023-12-05 18:42:05 +0700

    refactoring BrowserRouter

--- gen-23.09-react-routing/src/App.tsx
+++ gen-23.09-react-routing/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
@@ -7,28 +7,26 @@ import ProductSearch from "./pages/ProductSearch";
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Home />} />
-            <Route path="/profile" element={<div>profile</div>} />
-            <Route path="/wishlist" element={<div>wishlist</div>} />
-            <Route path="/cart" element={<div>cart</div>} />
-            <Route path="/checkout" element={<div>checkout</div>} />
-            <Route path="/order" element={<div>order</div>} />
-            <Route path="/search" element={<ProductSearch />} />
-            <Route path="/products">
-              <Route index element={<div> all product</div>} />
-              <Route path=":productId" element={<ProductDetail />} />
-            </Route>
-            <Route path="/login" element={<div>login</div>} />
-            <Route path="/register" element={<div>register</div>} />
-            <Route path="*" element={<div>404</div>} />
+    <Layout>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="/profile" element={<div>profile</div>} />
+          <Route path="/wishlist" element={<div>wishlist</div>} />
+          <Route path="/cart" element={<div>cart</div>} />
+          <Route path="/checkout" element={<div>checkout</div>} />
+          <Route path="/order" element={<div>order</div>} />
+          <Route path="/search" element={<ProductSearch />} />
+          <Route path="/products">
+            <Route index element={<div> all product</div>} />
+            <Route path=":productId" element={<ProductDetail />} />
           </Route>
-        </Routes>
-      </Layout>
-    </Router>
+          <Route path="/login" element={<div>login</div>} />
+          <Route path="/register" element={<div>register</div>} />
+          <Route path="*" element={<div>404</div>} />
+        </Route>
+      </Routes>
+    </Layout>
   );
 }
 
